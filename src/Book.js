0fb4bbc88114ddb4bcc501ebd94be1b7f57dcd7c
Chinyi <chinyi.cc@gmail.com>
2018-09-07 12:55:42 -0700

    make book.js component more readable

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,9 @@ class Book extends Component {
    return (
      <div className="search-books">
        <div className="search-books-bar">
+
          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+
          <div className="search-books-input-wrapper">
            {/*
              NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -16,12 +18,14 @@ class Book extends Component {
              you don't find a specific author or title. Every search is limited by search terms.
            */}
            <input type="text" placeholder="Search by title or author"/>
-
          </div>
+
        </div>
+
        <div className="search-books-results">
          <ol className="books-grid"></ol>
        </div>
+       
      </div>
    );
  }
