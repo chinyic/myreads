b3ce974e2ee128a33d7839e403422e99004b08be
Chin Yi Chen <chinyic@uoregon.edu>
2018-09-15 22:30:30 -0700

    search link

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 import Book from './Book';
 
 class MainPage extends Component {
@@ -79,7 +80,9 @@ render(){
         </div>
       </div>
       <div className="open-search">
-        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        <Link
+          to="/search"
+          >Add a book</Link>
       </div>
     </div>
   );
